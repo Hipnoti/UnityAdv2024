fdfd6f3c4665cd9b00c748ff703c7d01bc1f9c39
Lior Hadashian <lior@gavra-games.com>
2024-01-08 17:10:06 +0200

    modified CharacterController

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,9 +8,19 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private Transform targetDestination;
-    
+
     private void Start()
     {
         navMeshAgent.SetDestination(targetDestination.position);
+
+
+    }
+
+    private void Update()
+    {
+        if (!navMeshAgent.isStopped && navMeshAgent.remainingDistance <= 0.5f)
+        {
+            Debug.Log("Reached Destination!");
+        }
     }
 }
