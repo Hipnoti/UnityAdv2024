20eb798736c0e224339fe2133ce4857da31b3e5a
Lior Hadashian <lior@gavra-games.com>
2024-01-15 18:55:48 +0200

    Forgot to commit lesson changes :)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
+    private const int mudAreaID = 3;
+    
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private Transform[] pathWaypoints;
 
     private int currentWaypointIndex = 0;
     private void Start()
     {
+        navMeshAgent.SetAreaCost(mudAreaID, 0.2f);
         navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
     }
 
