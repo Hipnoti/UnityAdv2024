2a34f8f966bc2a1e509f348e7acac579f259c1ef
Lior Hadashian <lior@gavra-games.com>
2024-01-22 11:18:17 +0200

    Added animations + animator states

--- Assets/Scripts/Characters/CharacterController.cs
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -7,31 +7,45 @@ using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
-    private const string speedAnimatorParameter = "Speed"; 
+    private const string speedAnimatorParameter = "Speed";
+    private const int hurtLayerIndex = 1;
     private const int mudAreaID = 3;
-    private bool hasSpecialTevaNaot = true;
     
+    
+    [Header("Navigation")]
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private Transform[] pathWaypoints;
     [SerializeField] private Animator agentAnimator;
+    [SerializeField] private bool moveOnGameStart = false;
+
+    [Header("Health")] 
+    [SerializeField] private int maxHP;
+    [SerializeField] private int currentHP;
     
+    private bool hasSpecialTevaNaot = true;
     private int currentWaypointIndex = 0;
     private void Start()
     {
         if(hasSpecialTevaNaot)
            navMeshAgent.SetAreaCost(mudAreaID, 0.2f);
-        navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
+        if(moveOnGameStart)
+            navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
+        
+        currentHP = maxHP;
     }
 
     private void Update()
     {
         agentAnimator.SetFloat(speedAnimatorParameter, navMeshAgent.velocity.magnitude);
-        if (!navMeshAgent.isStopped && navMeshAgent.remainingDistance <= 0.1f)
+        if (moveOnGameStart && !navMeshAgent.isStopped && navMeshAgent.remainingDistance <= 0.1f)
         {
             currentWaypointIndex++;
             if (currentWaypointIndex >= pathWaypoints.Length)
                 currentWaypointIndex = 0;
             navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
         }
+
+        float healthPercent = 1 - (float)currentHP / maxHP;
+        agentAnimator.SetLayerWeight(hurtLayerIndex,healthPercent);
     }
 }
