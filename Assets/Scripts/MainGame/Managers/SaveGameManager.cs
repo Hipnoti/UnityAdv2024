394af63b66b1b3a0ca3ace46ab42b7f49c0be77c
Lior Hadashian <lior@gavra-games.com>
2024-04-01 20:46:42 +0300

    Added rotation

--- Assets/Scripts/MainGame/Managers/SaveGameManager.cs
+++ Assets/Scripts/MainGame/Managers/SaveGameManager.cs
@@ -17,33 +17,43 @@ public class SaveGameManager : MonoBehaviour
     public void SaveGame()
     {
         serializedSaveGame = new SerializedSaveGame();
-        serializedSaveGame.playerPosition = gameManager.playerCharacterController.transform.position;
-      // serializedSaveGame.playerPositionX = gameManager.playerCharacterController.transform.position.x;
-      // serializedSaveGame.playerPositionY = gameManager.playerCharacterController.transform.position.y;
-      // serializedSaveGame.playerPositionZ = gameManager.playerCharacterController.transform.position.z;
+        // serializedSaveGame.playerPosition = gameManager.playerCharacterController.transform.position;
+        // serializedSaveGame.playerRotation = gameManager.playerCharacterController.transform.eulerAngles;
+      serializedSaveGame.playerPositionX = gameManager.playerCharacterController.transform.position.x;
+      serializedSaveGame.playerPositionY = gameManager.playerCharacterController.transform.position.y;
+      serializedSaveGame.playerPositionZ = gameManager.playerCharacterController.transform.position.z;
+      
+      serializedSaveGame.playerRotationX = gameManager.playerCharacterController.transform.eulerAngles.x;
+      serializedSaveGame.playerRotationY = gameManager.playerCharacterController.transform.eulerAngles.y;
+      serializedSaveGame.playerRotationZ = gameManager.playerCharacterController.transform.eulerAngles.z;
+      
         serializedSaveGame.playerHP = gameManager.playerCharacterController.CurrentHP;
         serializedSaveGame.currentWaypointIndex = gameManager.playerCharacterController.currentWaypointIndex;
 
-         SaveToJson();
-             // SaveToBinary();
+         //SaveToJson();
+         SaveToBinary();
     }
 
     [ContextMenu("Load!")]
     public void LoadGame()
     {
-        LoadFromJson();
+        //LoadFromJson();
 
-      //  LoadFromBinary();
+        LoadFromBinary();
         
-        gameManager.playerCharacterController.transform.position = serializedSaveGame.playerPosition;
-       // gameManager.playerCharacterController.transform.position = new Vector3(serializedSaveGame.playerPositionX,
-       //     serializedSaveGame.playerPositionY, serializedSaveGame.playerPositionZ);
+        // gameManager.playerCharacterController.transform.position = serializedSaveGame.playerPosition;
+        // gameManager.playerCharacterController.transform.eulerAngles = serializedSaveGame.playerRotation;
+       gameManager.playerCharacterController.transform.position = new Vector3(serializedSaveGame.playerPositionX,
+           serializedSaveGame.playerPositionY, serializedSaveGame.playerPositionZ);
+       gameManager.playerCharacterController.transform.eulerAngles = new Vector3(serializedSaveGame.playerRotationX,
+           serializedSaveGame.playerRotationY, serializedSaveGame.playerRotationZ);
         gameManager.playerCharacterController.CurrentHP = serializedSaveGame.playerHP;
+        uiManager.RefreshHPText();
         gameManager.playerCharacterController.currentWaypointIndex = serializedSaveGame.currentWaypointIndex;
 
         gameManager.playerCharacterController.SetDestination();
 
-        uiManager.RefreshHPText();
+       
     }
 
     private void SaveToJson()
@@ -64,15 +74,19 @@ public class SaveGameManager : MonoBehaviour
         FileStream fileStream = new FileStream(Application.persistentDataPath + SAVE_FILE_NAME, FileMode.Create);
         BinaryFormatter converter = new BinaryFormatter();
         converter.Serialize(fileStream, serializedSaveGame);
+        fileStream.Close();
     }
 
     private void LoadFromBinary()
     {
-        FileStream fileStream = new FileStream(Application.persistentDataPath + SAVE_FILE_NAME, FileMode.Open);
+        if (File.Exists(Application.persistentDataPath + SAVE_FILE_NAME))
+        {
+            FileStream fileStream = new FileStream(Application.persistentDataPath + SAVE_FILE_NAME, FileMode.Open);
 
-        BinaryFormatter converter = new BinaryFormatter();
-        serializedSaveGame = converter.Deserialize(fileStream) as SerializedSaveGame;
+            BinaryFormatter converter = new BinaryFormatter();
+            serializedSaveGame = converter.Deserialize(fileStream) as SerializedSaveGame;
 
-        fileStream.Close();
+            fileStream.Close();
+        }
     }
 }
--- Assets/Scripts/MainGame/SerializedSaveGame.cs
+++ Assets/Scripts/MainGame/SerializedSaveGame.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 [System.Serializable]
 public class SerializedSaveGame
 {
-    public Vector3 playerPosition;
+    public float gameVersion;
+    // public Vector3 playerPosition;
+    // public Vector3 playerRotation;
     public uint playerHP;
     public int currentWaypointIndex;
 
-   // public float playerPositionX, playerPositionY, playerPositionZ;
+    public float playerPositionX, playerPositionY, playerPositionZ;
+    public float playerRotationX, playerRotationY, playerRotationZ;
+    
+    
 }
