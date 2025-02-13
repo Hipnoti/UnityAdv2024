10197dde400c4c5a52cb1ad63d64eb6f8baaaa8b
hippn <liorhadashian@gmail.com>
2025-02-13 23:08:08 +0200

    "Fix inconsistent indentation in MultiplyJob.cs"
    Adjusted indentation for better readability and code consistency in MultiplyJob.cs. This change improves code maintainability without altering the functionality.

--- Assets/Scripts/Jobs/MultiplyJob.cs
+++ Assets/Scripts/Jobs/MultiplyJob.cs
@@ -1,19 +1,18 @@
 ﻿using Unity.Burst;
-    using Unity.Jobs;
-    using Unity.Collections;
+using Unity.Jobs;
+using Unity.Collections;
 
-    public struct MultiplyJob : IJob
+public struct MultiplyJob : IJob
+{
+    public NativeArray<int> numbersToMultiply;
+    public NativeArray<int> result;
+
+    //Part of IJob
+    public void Execute()
     {
-        public NativeArray<int> numbersToMultiply;
-        public NativeArray<int> result;
-        
-        //Part of IJob
-        public void Execute()
+        for (int i = 0; i < numbersToMultiply.Length - 1; i++)
         {
-            for (int i = 0; i < numbersToMultiply.Length - 1; i++)
-            {
-                result[0] += numbersToMultiply[i] * numbersToMultiply[i + 1];
-            }
+            result[0] += numbersToMultiply[i] * numbersToMultiply[i + 1];
         }
     }
-    
+}
