43314a0b7f8dea74e5dc5e785ef7e1bb7ed47cd1
hippn <liorhadashian@gmail.com>
2025-02-13 20:49:53 +0200

    Add parallelized map generation job to improve performance
    Replaced ExampleJob with MapCreationJob for parallelized execution using IJobParallelFor. Updated JobManager to utilize the new job structure and added performance measurement to handle completion. Also, modified the "Audio Scene" Unity scene to integrate the updated JobManager.

--- Assets/Scripts/Jobs/JobManager.cs
+++ Assets/Scripts/Jobs/JobManager.cs
@@ -1,31 +1,60 @@
-﻿using Unity.Collections;
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
 
-namespace Jobs
+public class JobManager : MonoBehaviour
 {
+    private const int OBJECT_COUNT = 2000000000;
     
-    public class JobManager : MonoBehaviour
+    private JobHandle handle;
+    private NativeArray<float> result;
+    private Unity.Mathematics.Random random = new ((uint)DateTime.Now.Ticks);
+    
+    private IEnumerator Start()
     {
-        private JobHandle _handle;
-        private NativeArray<float> result;
+        yield return null;
         
-        [ContextMenu("Start Jobs")]
-        private void StartJobs()
-        {
-            ExampleJob job = new ExampleJob();
-            job.array = result = new NativeArray<float>(1, Allocator.TempJob);
-            job.number = 10;
-            job.number2 = 20;
-            job.number3 = 30;
-            _handle = job.Schedule();
-        }
+          StartJobs();
+          FinishJobs();
         
-        [ContextMenu("Finish Jobs")]
-        private void FinishJobs()
+        // Stopwatch stopwatch = Stopwatch.StartNew();
+        //  NomralMapCreation();
+        //  stopwatch.Stop();
+        // Debug.Log($"Normal Map Creation Time: {stopwatch.ElapsedMilliseconds/1000f} ms");
+    }
+
+    [ContextMenu("Start Jobs")]
+    private void StartJobs()
+    {
+        result = new NativeArray<float>(OBJECT_COUNT, Allocator.TempJob);
+        MapCreationJob job = new MapCreationJob { array = result, rand = random};
+        handle = job.Schedule(OBJECT_COUNT, 10000);
+    }
+
+    [ContextMenu("Finish Jobs")]
+    private void FinishJobs()
+    { 
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        handle.Complete();
+       
+        result.Dispose();
+        stopwatch.Stop();
+        Debug.Log($"Job Handle Completion Time: {stopwatch.ElapsedMilliseconds/1000f} ms");
+        
+    }
+    
+    private void NomralMapCreation()
+    {
+        float[] array = new float[OBJECT_COUNT];
+        for (int i = 0; i < array.Length; i++)
         {
-            _handle.Complete();
-            
+            array[i] = random.NextInt(1, 10);
         }
     }
 }
---
+++ Assets/Scripts/Jobs/MapCreationJob.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public struct MapCreationJob : IJobParallelFor
+{
+    public NativeArray<float> array;
+    public Unity.Mathematics.Random rand;
+
+    
+    public void Execute(int index)
+    {
+        array[index] = rand.NextInt(1, 10);
+    }
+}
