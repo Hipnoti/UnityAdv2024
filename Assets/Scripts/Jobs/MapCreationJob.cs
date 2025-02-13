917a8053b54877b4fee06e41e4f05a5296ea8ab2
hippn <liorhadashian@gmail.com>
2025-02-13 22:41:01 +0200

    Add MultiplyJob struct and update MapCreationJob
    Introduce MultiplyJob for pairwise multiplication using IJob. Refactor MapCreationJob to include Unity.Burst for optimization and prepare for potential thread-specific randomization logic, which remains commented out for now.

--- Assets/Scripts/Jobs/MapCreationJob.cs
+++ Assets/Scripts/Jobs/MapCreationJob.cs
@@ -1,15 +1,22 @@
-﻿using Unity.Collections;
+﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+
 
 public struct MapCreationJob : IJobParallelFor
 {
     public NativeArray<float> array;
     public Unity.Mathematics.Random rand;
-
     
     public void Execute(int index)
     {
         array[index] = rand.NextInt(1, 10);
     }
+    
+    // public void Execute(int index)
+    // {
+    //     Unity.Mathematics.Random threadRandom = new Unity.Mathematics.Random(15 + (uint)index);
+    //     array[index] = threadRandom.NextInt(1, 10);
+    // }
 }
