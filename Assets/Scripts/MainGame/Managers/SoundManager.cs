0759be75fd885bbda4237eb956a7d6f7c157b1ae
Lior Hadashian <liorhadashian@gmail.com>
2026-02-15 18:32:50 +0200

    **Refactored audio mixer structure and settings**
    Replaced the `Main Mixer` file with a new `Master` audio mixer setup, adding updated snapshots, effects, and parameter configurations (`CutoffPass`, `MasterVolume`). Adjusted references in the audio scene and `SoundManager` script to accommodate changes. Enhanced runtime control for master low-pass cutoff in `SoundManager`.

--- Assets/Scripts/MainGame/Managers/SoundManager.cs
+++ Assets/Scripts/MainGame/Managers/SoundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +8,29 @@ using UnityEngine.Serialization;
 public class SoundManager : MonoBehaviour
 {
     private const string MASTER_VOLUME_PARAMETER = "MasterVolume";
+
+    private const string CUT_OFF_LOW_PASS_PARAMTER = "CutoffPass";
    // private static string SFX_VOLUME_PARAMETER = "SFXVolume";
-    
+
+   [SerializeField] private float HZ;
     [SerializeField] private AudioMixerGroup masterMixer;
     // [SerializeField] private AudioMixerGroup sfxMixer;
     // [SerializeField] private AudioMixerGroup musicMixer;
 
-    // public void MasterVolumeSliderChanged(float newValue)
-    // {
-    //     float actualVolumeValue = (1 - newValue) * -40;
-    //     masterMixer.audioMixer.SetFloat(MASTER_VOLUME_PARAMETER, actualVolumeValue);
-    // }
+    public void MasterVolumeSliderChanged(float newValue)
+    {
+        float actualVolumeValue = (1 - newValue) * -40;
+        masterMixer.audioMixer.SetFloat(MASTER_VOLUME_PARAMETER, actualVolumeValue);
+    }
     
     // public void SFXVolumeSliderChanged(float newValue)
     // {
     //     float actualVolumeValue = (1 - newValue) * -40;
     //     mainMixer.audioMixer.SetFloat(SFX_VOLUME_PARAMETER, actualVolumeValue);
     // }
+
+    private void Update()
+    {
+        masterMixer.audioMixer.SetFloat(CUT_OFF_LOW_PASS_PARAMTER, HZ);
+    }
 }
