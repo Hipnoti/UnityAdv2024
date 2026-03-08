c4e112ceda7556cfefe82dbd5615de9f660a7ded
hippn <liorhadashian@gmail.com>
2026-03-08 18:18:22 +0200

    Add Addressables configuration and input system integration
    Added Addressable assets setup for managing bundles, schemas, and templates. Introduced Unity's generated Input System file for streamlined input handling and interaction.

--- Assets/Scripts/MainGame/Managers/LevelManager.cs
+++ Assets/Scripts/MainGame/Managers/LevelManager.cs
@@ -11,19 +11,29 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private Transform targetTransform;
 
     private GameObject loadedSector;
+    private GameObject spawnedSector;
     private AsyncOperationHandle<GameObject> loadedSectorHandle;
     
+    [ContextMenu("Load and Generate Sector")]
     public void LoadAndGenerateSector()
     {
         AsyncOperationHandle<GameObject> asyncOperation = Addressables.InstantiateAsync(sectorAsset, targetTransform.position, Quaternion.identity);
         asyncOperation.Completed +=  AsyncOperationOnCompleted;
     }
-
+    
     private void AsyncOperationOnCompleted(AsyncOperationHandle<GameObject> obj)
     {
        Debug.Log("Instantiate");
+       spawnedSector = obj.Result;
     }
-
+    
+    
+    [ContextMenu("Release Sector")]
+    private void ReleaseSector()
+    {
+        Addressables.ReleaseInstance(spawnedSector);
+    }
+    
     [ContextMenu("Load Sector")]
     public void LoadSector()
     {
@@ -33,20 +43,20 @@ public class LevelManager : MonoBehaviour
     
     private void LoadAsyncComplete(AsyncOperationHandle<GameObject> asyncOperationHandle)
     {
-        Debug.Log("Loading complete!");
         if(asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
         {
+            Debug.Log("Loading complete!");
             loadedSector = asyncOperationHandle.Result;
             loadedSectorHandle = asyncOperationHandle;
         }
     }
-
+    //
     [ContextMenu("Instantiate Sector")]
     private void InstantiateSector()
     {
         GameObject instadSector = Instantiate(loadedSector, targetTransform.position, Quaternion.identity);
     }
-    //
+  
     [ContextMenu("Unload Sector")]
     private void UnloadSector()
     {
